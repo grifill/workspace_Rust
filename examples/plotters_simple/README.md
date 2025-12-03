5b5ab2f69554c320380357c333c8212e46b9812c
Gregory Filatov <grigorijfilatov@gmail.com>
2025-12-03 21:59:10 +0300

    plotter example

---
+++ examples/plotters_simple/README.md
@@ -0,0 +1,23 @@
+# Simple project, plotters library usage
+____
+
+## Main site
+
+```bash
+    https://plotters-rs.github.io/book/intro/introduction.html
+```
+
+## Install prerequisite libraries to system
+
+```bash
+    sudo apt install pkg-config libfreetype6-dev libfontconfig1-dev
+```
+
+## Add dependency to cargo.toml
+
+```bash
+    [dependencies]
+    plotters = "0.3.3"
+```
+
+____
---
+++ examples/plotters_simple/src/main.rs
@@ -0,0 +1,20 @@
+use plotters::prelude::*;
+
+fn main() {
+  let root_area = BitMapBackend::new("./images/LinePlotDemo.png", (640, 480))
+    .into_drawing_area();
+  root_area.fill(&WHITE).unwrap();
+
+  let mut ctx = ChartBuilder::on(&root_area)
+    .set_label_area_size(LabelAreaPosition::Left, 40)
+    .set_label_area_size(LabelAreaPosition::Bottom, 40)
+    .caption("Line Plot Demo", ("sans-serif", 40))
+    .build_cartesian_2d(-10..10, 0..100)
+    .unwrap();
+
+  ctx.configure_mesh().draw().unwrap();
+
+  ctx.draw_series(
+    LineSeries::new((-10..=10).map(|x| (x, x* x)), &GREEN)
+  ).unwrap();
+}
