d62e60681cac4d1216048163325327e31cf9f3bc
Gregory Filatov <grigorijfilatov@gmail.com>
2026-01-14 20:05:37 +0300

    edit main

--- games/cubesnum/src/main.rs
+++ games/cubesnum/src/main.rs
@@ -1,3 +1,11 @@
+use miniquad::conf;
+
 fn main() {
-    println!("Hello, world!");
+    let conf = conf::Conf {
+        window_title: "Rust Cubes Demo".to_string(),
+        window_width: 1920,
+        window_height: 1080,
+        ..Default::default()
+    };
+    //miniquad::start(conf, /* f */);
 }
