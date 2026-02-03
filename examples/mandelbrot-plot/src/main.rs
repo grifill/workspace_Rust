8cafa356329ea5f3590acfa30fd4ca3c6ef89467
grifill <grigorijfilatov@gmail.com>
2026-02-03 13:56:14 +0300

    heart main

--- examples/mandelbrot-plot/src/main.rs
+++ examples/mandelbrot-plot/src/main.rs
@@ -1,27 +1,22 @@
-fn main() {
-    println!("Hello, world!");
-}
-
-fn square_loop(mut x: f64) {
-    loop {
-        x = x * x;
-    }
-}
-
-fn square_add_loop(c: f64) {
-    let mut x = 0.;
-    loop {
-        x = x * x + c;
-    }
-}
-
+// Crates: 
+// Num
 extern crate num;
 use num::Complex;
 
-#[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>) { 
+fn main() {
+    println!("Hello, Mandelbrot!");
+}
+
+/// Try to determine whether 'c' belongs to the Mandelbrot set, limiting the number of iterations 'limit'
+/// If 'c' does not belong to the Mandelbrot set, return 'Some(i)', where 'i' - number of iterations
+/// If 'c' can belong to the Mandelbrot set, return 'None'
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> { 
     let mut z = Complex { re: 0.0, im: 0.0 }; 
-    loop { 
-        z = z * z + c;
-    }
+    for i in 0..limit { 
+        z = z*z + c; 
+        if z.norm_sqr() > 4.0 { 
+            return Some(i); 
+        } 
+    } 
+    None
 }
