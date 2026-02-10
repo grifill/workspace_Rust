94162a564fc1d1ab5b86c46873ece9d16d2d5115
grifill <grigorijfilatov@gmail.com>
2026-02-10 18:48:40 +0300

    pixel_to_point

--- examples/mandelbrot-plot/src/main.rs
+++ examples/mandelbrot-plot/src/main.rs
@@ -65,3 +65,31 @@ fn test_parse_complex() {
     Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",-0.0625"), None);
 }
+
+
+/// Given the row and column of a pixel in the output image, 
+/// returns the corresponding point in the complex plane. 
+/// 'bounds' is a pair defining the width and height of the image in pixels. 
+/// 'pixel' is a (row, column) pair defining a specific pixel in the image. 
+/// The 'upper_left' and 'lower_right' parameters are points in the complex plane describing the area covered by the image
+fn pixel_to_point(bounds: (usize, usize), 
+                  pixel: (usize, usize), 
+                  upper_left: Complex<f64>, 
+                  lower_right: Complex<f64>)
+                  -> Complex<f64> {
+                    
+    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+    Complex {
+        /// pixel.1 increases as you move down, while the imaginary part increases as you move up
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64 
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75), 
+                     Complex { re: -1.0, im: 1.0 }, 
+                     Complex { re: 1.0, im: -1.0 }), 
+                     Complex { re: -0.5, im: -0.5 });
+}
