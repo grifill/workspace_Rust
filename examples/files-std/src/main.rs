f00d419ebb914deaf2577acc28c6781df2c5bb93
grifill <grigorijfilatov@gmail.com>
2026-03-18 19:34:35 +0300

    exam

--- examples/files-std/src/main.rs
+++ examples/files-std/src/main.rs
@@ -1,18 +1,31 @@
 use std::fs::File;
-use std::io::{Write};
+use std::io::Write;
 
 fn main() -> Result<(), std::io::Error> {
-    
     let string_name_file = "exampleFile.txt";
     let string_data_file = String::from("Hello std::File");
-    
+
     // Create File
     let mut file = File::create(string_name_file)?;
-    
-    // Write string 
+
+    // Write string
     file.write_all(string_data_file.as_bytes())?;
-    
-    // Print message 
+
+    // Print message
     println!("File {} has been written succesfully", string_name_file);
     Ok(())
 }
+
+/*fn main() -> Result<(), std::io::Error> {
+
+    let file_path = "example.txt";  // файл для записи
+    let text = String::from("Hello METANIT.COM!");    // текст для записи
+    let mut output_file = File::create(file_path)?; // создаем файл
+    output_file.write_all(text.as_bytes())?;     // записываем в файл текст
+
+    let mut content = String::new();    // строка для считывания файла
+    let mut input_file = File::open(file_path)?;   // открываем файл
+    input_file.read_to_string(&mut content)?;      // считываем содержимое файла в строку
+    println!("File content: {}", content);      // выводим считанный текст на консоль
+    Ok(())
+}*/
