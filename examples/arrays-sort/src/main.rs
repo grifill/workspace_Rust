7effbf30b3262cd6de5db864a41baa243bbce745
grifill <grigorijfilatov@gmail.com>
2026-08-26 19:51:15 +0300

    target rename

--- examples/arrays-sort/src/main.rs
+++ examples/arrays-sort/src/main.rs
@@ -6,7 +6,7 @@ mod merge_sort;
 
 fn main() {
     let mut arr = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-    let target = 7;
+    let target = 3;
 
     let mut rng = rand::rng();
     let m: u32 = rng.random_range(0..100);
